6afdc88dccb166a1362e4ce8e0874f8bf102e301
Robin Thomas <robinthomas2591@gmail.com>
2019-01-11 04:58:06 +0800

    display the wallet not connected banner

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
+app.use(express.json());
+app.options('*', cors());
+app.use(express.static(__dirname + '/static'));
+
 app.listen(port, () => console.log(`app listening on ${port}`));
