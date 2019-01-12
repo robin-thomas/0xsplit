1c771f29c822f6c239d2fd698ec5048e60084a47
Robin Thomas <robinthomas2591@gmail.com>
2019-01-12 10:41:23 +0800

    display wallet address after connect

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,15 @@ const Session = require('./modules/session.js');
 let token = null;
 
 $(document).ready(() => {
-  const walletLoginButton = $('#wallet-login-button'),
-        confirmAddrButton = $('#confirm-eth-addr'),
-        walletLeftConnect = $('#wallet-left-connect');
+  const walletLoginButton    = $('#wallet-login-button'),
+        walletLogoutButton   = $('#wallet-logout-button'),
+        walletAddressDisplay = walletLogoutButton.find('.wallet-label-bottom'),
+        confirmAddrButton    = $('#confirm-eth-addr'),
+        walletLeftConnect    = $('#wallet-left-connect');
+
+  const walletBeforeConnect = $('#wallet-before-connect'),
+        walletConnect       = $('#wallet-connect'),
+        walletAfterConnect  = $('#wallet-after-connect');
 
   const walletAddreses = $('#eth-addresses');
 
@@ -26,20 +32,28 @@ $(document).ready(() => {
       alert(err.message);
     }
   };
-
-  walletLoginButton.on('click', async (e) => walletConnectHandler(e));
-  walletLeftConnect.on('click', async (e) => walletConnectHandler(e));
-
-  confirmAddrButton.on('click', async (e) => {
+  const walletConnectConfirmHandler = async (e) => {
     const address = walletAddreses.val();
     const message = 'Signing this message proves to us you are in control of your account while never storing any sensitive account information.';
 
     try {
       if (await Session.login(address, message)) {
+        const addressDisplay = address.substr(0, 5) + '...' + address.substr(37);
+        walletAddressDisplay.text(addressDisplay);
         walletConnectDialog.modal('hide');
+        walletBeforeConnect.fadeOut();
+        walletConnect.fadeOut();
+        walletAfterConnect.fadeIn();
+
+        walletLoginButton.fadeOut();
+        walletLogoutButton.css('display', 'flex').hide().fadeIn();
       }
     } catch (err) {
       alert(err);
     }
-  });
+  };
+
+  walletLoginButton.on('click', async (e) => walletConnectHandler(e));
+  walletLeftConnect.on('click', async (e) => walletConnectHandler(e));
+  confirmAddrButton.on('click', async (e) => walletConnectConfirmHandler(e));
 });
